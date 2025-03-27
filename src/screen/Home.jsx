369f9121ae8ed269a1dcee33d22cca83503060b6
Bezawit Megersa <134366613+bezawit11234@users.noreply.github.com>
2025-03-27 10:11:14 +0300

    add tailwind css

---
+++ src/screen/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-blue-500 text-xl font-bold mt-10 font-serif'>
+      hello react
+    </div>
+  )
+}
+
+export default Home
